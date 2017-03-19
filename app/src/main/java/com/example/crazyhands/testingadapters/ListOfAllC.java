81384388e541aeea5924770944009a5f7c8f6330
crazyhands <ian.leedham21@gmail.com>
2017-03-19 23:21:24 +0000

    change activities to fragments within activities

--- app/src/main/java/com/example/crazyhands/testingadapters/ListOfAllC.java
+++ app/src/main/java/com/example/crazyhands/testingadapters/ListOfAllC.java
@@ -17,36 +17,10 @@ public class ListOfAllC extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_of_all_c);
+        setContentView(R.layout.activity_catagory);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new ListOfAllCFragment())
+                .commit();
 
-        // Create an ArrayList of ClistItems objects
-        final ArrayList<List_item> ListItems = new ArrayList<List_item>();
-        ListItems.add(new List_item("YOGA", "8.00", YogaInfo.class));
-        ListItems.add(new List_item("PILaties", "9.00", PilatiesInfo.class));
-
-
-        // Create an {@link SimpleListAdapter}, whose data source is a list of
-        // {@link List_item}s. The adapter knows how to create list item views for each item
-        // in the list.
-        SimpleListAdapter ItemsAdapter = new SimpleListAdapter(this, ListItems);
-
-        // Get a reference to the ListView, and attach the adapter to the listView.
-        ListView listView = (ListView) findViewById(R.id.listview_Item);
-        listView.setAdapter(ItemsAdapter);
-
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                List_item item = ListItems.get(position);
-
-                Intent newIntent = new Intent(ListOfAllC.this, item.getActivityToName());
-                startActivity(newIntent);
-
-                //mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
-
-
-            }
-        });
     }
 }
