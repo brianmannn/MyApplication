bfd9984f9214b62d0b56995ab6e7cee8bb338abd
crazyhands <ian.leedham21@gmail.com>
2017-03-19 20:32:05 +0000

    second commit with list item layout, list_item.java and SimpleListItemAdapter.java

---
+++ app/src/main/java/com/example/crazyhands/testingadapters/List_item.java
@@ -0,0 +1,19 @@
+package com.example.crazyhands.testingadapters;
+
+/**
+ * Created by crazyhands on 19/03/2017.
+ */
+
+public class List_item {
+
+    private String mCname;
+
+    public List_item(String CName){
+        mCname = CName;
+    }
+    public String getCname() {
+        return mCname;
+    }
+
+
+}
