38e7d4bb0dfcfe0c430c1fe157150e2d75dfda8d
crazyhands <ian.leedham21@gmail.com>
2017-03-19 21:16:27 +0000

    added onclick function to all the items with just a toast message

--- app/src/main/java/com/example/crazyhands/testingadapters/ListOfAllC.java
+++ app/src/main/java/com/example/crazyhands/testingadapters/ListOfAllC.java
@@ -1,8 +1,13 @@
 package com.example.crazyhands.testingadapters;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,10 +19,9 @@ public class ListOfAllC extends AppCompatActivity {
         setContentView(R.layout.activity_list_of_all_c);
 
         // Create an ArrayList of ClistItems objects
-        ArrayList<List_item> ListItems = new ArrayList<List_item>();
+        final ArrayList<List_item> ListItems = new ArrayList<List_item>();
         ListItems.add(new List_item("Donut", "8.00"));
         ListItems.add(new List_item("Eclair", "9.00"));
-
 
 
         // Create an {@link SimpleListAdapter}, whose data source is a list of
@@ -28,5 +32,24 @@ public class ListOfAllC extends AppCompatActivity {
         // Get a reference to the ListView, and attach the adapter to the listView.
         ListView listView = (ListView) findViewById(R.id.listview_Item);
         listView.setAdapter(ItemsAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                List_item item = ListItems.get(position);
+
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                //mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
+
+
+
+            }
+        });
     }
 }
